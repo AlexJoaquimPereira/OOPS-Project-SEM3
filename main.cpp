db500b20b4d316e242f37671b2adc2803365e729
Alex Joaquim Pereira <alexpereira1781@gmail.com>
2023-10-25 10:56:25 +0530

    Update main.cpp
    Added the menu direcly in the main() function
    Added an infinite loop to prevent program from just closing after it is done

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
 #include "uimanip.cpp"
+#include "oopsmodified.cpp"
 using namespace std;
 
 int main(){
-    int s = 0;
-    clrscr();
+    Student s; // Should we work with only one obj, or make a list of objects?
+    int choice = 0;
+    clrscr;
     do{
-        cout << header;
-        cout << menu;
-        cin >> s;
-        clrscr();
-        cout << header;
-        switch(s){ // Add the cases linking to class student or class file functions
-
+        header();
+        cout << "MENU: \n";
+        cout << "-----------------------------------------------------------------------------\n";
+        cout << "[1] Enter student details\n";
+        cout << "[2] Edit age of a student\n";
+        cout << "[3] Append the address to a student file\n";
+        cout << "[4] Exit\n";
+        cout << "-----------------------------------------------------------------------------\n";
+        cout << "Choose your option: ";
+        cin >> choice;
+        clrscr;
+        header();
+        switch(choice){ 
+            case 1:
+            case 2:
+            case 3:
+            case 4: break;
+            default: cout << "Invalid input\n";
         }
-        clrscr();
-    }while(s!=5);
+        clrscr;
+    }while(choice != 4);//Change to whatever is maximum
     
-    cout << "Thanks for interacting\n";// Ending screen, can add a thank you page
-    cin.get(); // To prevent .exe/.out file from just stopping
+    header();
+    cout << "Thanks for interacting!\n";// Ending screen, can add a thank you page
+    while(1); // To prevent .exe/.out file from just stopping
 }
